e87d647d43b30e604890210cf36b76808e3408bc
tanishabisht <agamyatani@gmail.com>
2021-12-06 02:58:18 +0530

    fix :: theme

--- components/Home/Tech.js
+++ components/Home/Tech.js
@@ -7,7 +7,6 @@ const Tech = () => {
             <h1 className="header techContainer__header">Tech I have worked with</h1>
             <div className="techContainer__socialHandles">
                 {stacks.map((e,k) => {
-                    console.log(e.icon)
                     return (
                         <span data-text={e.name} className={`techContainer__socialHandles__tooltip ${e.isFav?"icon_fav":null}`} key={k}>
                             <span><Image src={e.icon} height={20} width={20} /></span>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,45 @@
-import Link from 'next/link'
-import Image from 'next/image'
-import { useTheme } from 'next-themes'
+import Link from "next/link"
+import Image from "next/image"
+import { useTheme } from "next-themes"
+import { useEffect } from "react"
 
 const Navbar = () => {
-    const { theme, setTheme } = useTheme()
+	const { theme, setTheme } = useTheme()
 
-    const changeTheme = () => {
-        if(theme==="light") setTheme('dark')
-        else setTheme('light')
-    }
-    
-    return (
-        <nav className="nav">
-            <div className="nav__logo">
-                <Link href='/'><a><Image src='/favicon.ico' width={50} height={50} /></a></Link>
-                <span>TANISHA</span>
-            </div>
-            <Link href='/works'><a>works</a></Link>
-            <Link href='https://github.com/tanishabisht'><a target="_blank">github</a></Link>
-            <span onClick={changeTheme} className="nav__themeToggle">
-                {theme==="light" ? <i className="fas fa-sun"></i> : <i className="fas fa-moon"></i>}
-            </span>
-        </nav>
-    )
+	useEffect(() => {
+		setTheme("light")
+	}, [])
+
+	const changeTheme = () => {
+		if (theme === "light") setTheme("dark")
+		else setTheme("light")
+	}
+
+	return (
+		<nav className="nav">
+			<div className="nav__logo">
+				<Link href="/">
+					<a>
+						<Image src="/favicon.ico" width={50} height={50} />
+					</a>
+				</Link>
+				<span>TANISHA</span>
+			</div>
+			<Link href="/works">
+				<a>works</a>
+			</Link>
+			<Link href="https://github.com/tanishabisht">
+				<a target="_blank">github</a>
+			</Link>
+			<span onClick={changeTheme} className="nav__themeToggle">
+				{theme === "light" ? (
+					<i className="fas fa-moon"></i>
+				) : (
+					<i className="fas fa-sun"></i>
+				)}
+			</span>
+		</nav>
+	)
 }
 
 export default Navbar
